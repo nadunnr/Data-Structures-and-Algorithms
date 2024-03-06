27ded981ced49d3b66e0310f8505b8dcc045d4bd
nadunnr <naveendra0729@gmail.com>
2024-03-06 20:05:45 +0530

    add traversal methods

--- Data Structures/binary_search_tree.cpp
+++ Data Structures/binary_search_tree.cpp
@@ -22,15 +22,63 @@ private:
             return new Node(value);
         }
 
-        else if (node->data < value){
+        else if (node->data > value){
             node->left =  insert_node(node->left, value);
         }
 
-        else if (node->data >= value){
+        else if (node->data <= value){
             node->right = insert_node(node->right, value);
         }
 
         return node;
+    }
+
+    bool search_node(Node* node,int value){
+
+        if (node == nullptr){
+            return false;
+        }
+
+        else if (node->data == value){
+            return true;
+        }
+
+        else if (node->data > value){
+            return search_node(node->left, value);
+        }
+
+        else if (node->data < value){
+            return search_node(node->right, value);
+        }
+    }
+
+    void inorder_traversal(Node* node){
+        
+        if (node != nullptr){
+
+            inorder_traversal(node->left);
+            cout <<node->data<<" ";
+            inorder_traversal(node->right);
+        }
+    }
+
+    void preorder_traversal(Node* node){
+        
+        if (node != nullptr){
+            cout <<node->data<<" ";
+            preorder_traversal(node->left);
+            preorder_traversal(node->right);
+        }
+    }
+
+    void postorder_traversal(Node* node){
+        
+        if (node != nullptr){
+
+            postorder_traversal(node->left);
+            postorder_traversal(node->right);
+            cout <<node->data<<" ";
+        }
     }
 
 public:
@@ -48,31 +96,20 @@ public:
         return root;
     }
 
-    bool search_node(Node* node,int value){
-
-        if (node == nullptr){
-            return false;
-        }
-
-        else if (node->data == value){
-            return true;
-        }
-
-        else if (node->data < value){
-            return search_node(node->left, value);
-        }
-
-        else if (node->data > value){
-            return search_node(node->right, value);
-        }
-    }
-
     bool search(int val){
         return search_node(root, val);
     }
 
+    void traversal(string mode){
+
+        if (mode == "inorder") inorder_traversal(root);
+        else if (mode == "preorder") preorder_traversal(root);
+        else if (mode == "postorder") postorder_traversal(root);
+        cout<<endl;
+    }
+
     bool delete_node(int);
-    void traversal();
+    
 };
 
 bool BinarySearchTree::is_empty(){
@@ -95,4 +132,5 @@ int main(){
     cout<<BST.search(6)<<endl;
     cout<<BST.search(7)<<endl;
     cout<<BST.search(9)<<endl;
+    BST.traversal("inorder");
 }
