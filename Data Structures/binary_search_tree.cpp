61d305dfb34b16a7d6a4cc2c361ea56c828562f1
nadunnr <naveendra0729@gmail.com>
2024-03-06 13:55:35 +0530

    add is_empty()

--- Data Structures/binary_search_tree.cpp
+++ Data Structures/binary_search_tree.cpp
@@ -1,3 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Node{
+    int data;
+    Node* left = nullptr;
+    Node* right = nullptr;
+};
+
+class BST{
+
+private: 
+    Node* root = nullptr;
+
+public:
+    BST(){
+        root = NULL;
+    }
+
+    void insert_node(int);
+    bool is_empty();
+    int search(int);
+    bool delete_node(int);
+    void travers();
+
+};
+
+bool BST::is_empty(){
+    if (root == nullptr) return true;
+    else return false;
+}
+
